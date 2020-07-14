27be009f116fd54d62142f56fc3e7accdd8e21e0
arkapravamahapatra <arkapravamahapatra01@gmail.com>
2020-07-14 15:00:34 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter first and second number");
+            a = Convert.ToInt16(Console.ReadLine());
+            b = Convert.ToInt16(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("After swapping");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
